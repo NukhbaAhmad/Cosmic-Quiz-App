9fa28e646f80d5476b7cf272178ce48a1383e565
NukhbaAhmad <nukhbaahmadali@gmail.com>
2026-03-28 23:43:22 +0500

    Fix/label not being shown when no ranges found from default range list

--- src/types/quiz.ts
+++ src/types/quiz.ts
@@ -1,7 +1,7 @@
 type Option = { label: string; score: number }
 type Question = { id: number; question: string; options: Option[] }
 type Result = { range: [number, number]; label: string }
-type Range = { minScore: number; maxScore: number; createdAt: string; label: string }
+type Range = { minScore: number; maxScore: number; createdAt?: string; label: string }
 type ScoreRecord = { id: string; score: number; label: string; notes: string; createdAt: string }
 interface QuizData {
   quizTitle: string
--- src/utils/quiz.utils.ts
+++ src/utils/quiz.utils.ts
@@ -1,11 +1,10 @@
 import { DEFAULT_SCORING_RANGES } from '@/lib/default-ranges'
 import { Range } from '@/types/quiz'
 
-export const getResultLabel = (
-  score: number,
-  ranges: Range[] | typeof DEFAULT_SCORING_RANGES = DEFAULT_SCORING_RANGES,
-) => {
+export const getResultLabel = (score: number, ranges: Range[] = DEFAULT_SCORING_RANGES) => {
   if (score === 13) return 'You lucky fucker! You scored 13 exactly.'
-  const res = ranges.find(({ minScore, maxScore }) => score >= minScore && score <= maxScore)
+  const ranglesToUse = ranges && ranges.length > 0 ? ranges : DEFAULT_SCORING_RANGES
+
+  const res = ranglesToUse.find(({ minScore, maxScore }) => score >= minScore && score <= maxScore)
   return res ? res.label : 'No result'
 }
